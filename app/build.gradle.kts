fe224f550df8d6633596264a954081dda839af4e
Nelson Silva <nelsondsd@gmail.com>
2026-08-06 07:42:38 +0200

    chore: bump version to 1.5 (15)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = 27
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.4"
+        versionCode = 15
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
